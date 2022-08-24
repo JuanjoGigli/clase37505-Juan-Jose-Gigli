932f10a9cb764af28494d394f1ee0c77c388339c
Juanjo Gigli <cachirli20@gmail.com>
2022-08-24 02:26:42 -0300

    nuevo proyecto

---
+++ js/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Iphone 11 morado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: './img/ipgone11m2.jpg'},
+    {id: 2, nombre: "Iphone 11 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: './img/iphone11b2.jpg'},
+    {id: 3, nombre: "Iphone 12 morado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 800,  img: './img/iphone12m2.jpg'},
+    {id: 4, nombre: "Iphone 12 rojo", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 800,  img: './img/iphone12r2.jpg'},
+    {id: 5, nombre: "Iphone 13 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 1000,  img: './img/iphone13b2.jpg'},
+    {id: 6, nombre: "Iphone 13 dorado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 1000,  img: './img/iphone13d2.jpg'},
+    {id: 7, nombre: "Iphone 10 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 500,  img: './img/iphone10b.jpg'},
+    {id: 8, nombre: "Iphone 10 negro", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 500,  img: './img/iphone10n.jpg'},
+] 
