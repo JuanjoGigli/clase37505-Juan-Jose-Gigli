81a83136edfc7aa233b1962a0d33b6ff71714e8f
Juanjo Gigli <cachirli20@gmail.com>
2022-09-14 19:07:32 -0300

    segunda fase final

--- js/stock.js
+++ js/stock.js
@@ -1,6 +1,6 @@
 let stockProductos = [
-    {id: 1, nombre: "Iphone 11 morado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: '../img/ipgone11m2.jpg'},
-    {id: 2, nombre: "Iphone 11 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: '../img/iphone11b2.jpg'},
+    {id: 1, nombre: "Iphone 11 morado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: './img/ipgone11m2.jpg'},
+    {id: 2, nombre: "Iphone 11 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 600,  img: './img/iphone11b2.jpg'},
     {id: 3, nombre: "Iphone 12 morado", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 800,  img: './img/iphone12m2.jpg'},
     {id: 4, nombre: "Iphone 12 rojo", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 800,  img: './img/iphone12r2.jpg'},
     {id: 5, nombre: "Iphone 13 blanco", tipo: "iphone", cantidad: 1, desc: "Los mejores precios para vos!", precio: 1000,  img: './img/iphone13b2.jpg'},
